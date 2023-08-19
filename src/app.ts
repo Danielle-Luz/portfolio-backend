8dba5b15b97753c37b4596ac7438275fabc182e6
Danielle Luz <daniln2209@gmail.com>
2023-08-19 19:32:32 -0300

    feat: associate error handler to all routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import "express-async-errors";
+import { errorHandler } from "./errors";
 
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.user(errorHandler);
 
 export { app };
---
+++ src/errors/index.ts
@@ -0,0 +1,4 @@
+import { AppError } from "./AppError";
+import { errorHandler } from "./errorHandler";
+
+export { AppError, errorHandler };
