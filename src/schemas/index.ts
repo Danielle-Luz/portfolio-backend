4d2c40fb904a26f938fcb0c80302cc1877b02e03
Danielle Luz <daniln2209@gmail.com>
2023-08-19 15:32:18 -0300

    feat: add schemas import in index file

---
+++ src/schemas/index.ts
@@ -0,0 +1,15 @@
+import { newTechnologySchema, readTechnologySchema } from "./technologies";
+import { newProjectSchema, readProjectSchema } from "./projects";
+import {
+  newDevelopmentExperience,
+  readDevelopmentExperience,
+} from "./developmentExperiences";
+
+export {
+  newDevelopmentExperience,
+  readDevelopmentExperience,
+  newProjectSchema,
+  readProjectSchema,
+  newTechnologySchema,
+  readTechnologySchema,
+};
