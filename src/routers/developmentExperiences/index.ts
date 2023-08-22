cb1693cd13d04ebb6002b1b3b8ce273839f42d89
Danielle Luz <daniln2209@gmail.com>
2023-08-22 01:06:22 -0300

    feat: add middlewares in development experiences routes

--- src/routers/developmentExperiences/index.ts
+++ src/routers/developmentExperiences/index.ts
@@ -1,24 +1,36 @@
 import { Router } from "express";
 import { DevelopmentExperiencesController } from "../../controllers";
 import { app } from "../../app";
+import {
+  DevelopmentExperiencesMiddlewares,
+  UtilsMiddlewares,
+} from "../../middlewares";
 
 const developmentExperienceRouter = Router();
 
-developmentExperienceRouter.post("/", DevelopmentExperiencesController.create);
+developmentExperienceRouter.post(
+  "/",
+  DevelopmentExperiencesMiddlewares.validateNewDevelopmentExperience,
+  DevelopmentExperiencesController.create
+);
 
 developmentExperienceRouter.get("/", DevelopmentExperiencesController.getAll);
 developmentExperienceRouter.get(
   "/:type",
+  DevelopmentExperiencesMiddlewares.validateValueAsExperienceType,
   DevelopmentExperiencesController.getByType
 );
 
 developmentExperienceRouter.patch(
   "/:id",
+  UtilsMiddlewares.validateId,
+  DevelopmentExperiencesMiddlewares.validateUpdatedDevelopmentExperience,
   DevelopmentExperiencesController.update
 );
 
 developmentExperienceRouter.delete(
   "/:id",
+  UtilsMiddlewares.validateId,
   DevelopmentExperiencesController.delete
 );
 
