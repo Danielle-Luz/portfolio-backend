a2e93349a2f6ebdb93e4d843f6bb2c169103459c
Danielle Luz <daniln2209@gmail.com>
2023-08-22 01:11:54 -0300

    feat: add middlewares in technologies routes

--- src/routers/technologies/index.ts
+++ src/routers/technologies/index.ts
@@ -1,16 +1,34 @@
 import { Router } from "express";
 import { TechnologiesController } from "../../controllers";
 import { app } from "../../app";
+import { TechnologiesMiddlewares, UtilsMiddlewares } from "../../middlewares";
 
 const technologyRouter = Router();
 
-technologyRouter.post("/", TechnologiesController.create);
+technologyRouter.post(
+  "/",
+  TechnologiesMiddlewares.validateNewTechnology,
+  TechnologiesController.create
+);
 
-technologyRouter.patch("/:id", TechnologiesController.update);
+technologyRouter.patch(
+  "/:id",
+  UtilsMiddlewares.validateId,
+  TechnologiesMiddlewares.validateUpdatedTechnology,
+  TechnologiesController.update
+);
 
 technologyRouter.get("/", TechnologiesController.getAll);
-technologyRouter.get("/:id", TechnologiesController.getOne);
+technologyRouter.get(
+  "/:id",
+  UtilsMiddlewares.validateId,
+  TechnologiesController.getOne
+);
 
-technologyRouter.delete("/:id", TechnologiesController.delete);
+technologyRouter.delete(
+  "/:id",
+  UtilsMiddlewares.validateId,
+  TechnologiesController.delete
+);
 
 app.use("/technologies", technologyRouter);
