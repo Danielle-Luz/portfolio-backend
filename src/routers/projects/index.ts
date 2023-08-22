add1da74028053873e5a899426c77397575f17f0
Danielle Luz <daniln2209@gmail.com>
2023-08-22 01:10:00 -0300

    feat: add middlewares in projects routes

--- src/routers/projects/index.ts
+++ src/routers/projects/index.ts
@@ -1,20 +1,47 @@
 import { Router } from "express";
 import { ProjectsController } from "../../controllers";
 import { app } from "../../app";
+import { ProjectsMiddlewares, UtilsMiddlewares } from "../../middlewares";
 
 const projectRouter = Router();
 
-projectRouter.post("/", ProjectsController.create);
-projectRouter.post("/:id/technologies", ProjectsController.addTechnology);
+projectRouter.post(
+  "/",
+  ProjectsMiddlewares.validateNewProject,
+  ProjectsController.create
+);
+projectRouter.post(
+  "/:id/technologies",
+  UtilsMiddlewares.validateId,
+  UtilsMiddlewares.validateBodyParameterId,
+  ProjectsController.addTechnology
+);
 
 projectRouter.get("/", ProjectsController.getAll);
-projectRouter.get("/:id", ProjectsController.getOne);
-projectRouter.get("/:stack", ProjectsController.getByStack);
+projectRouter.get(
+  "/:id",
+  UtilsMiddlewares.validateId,
+  ProjectsController.getOne
+);
+projectRouter.get(
+  "/:stack",
+  UtilsMiddlewares.validateValueAsStack,
+  ProjectsController.getByStack
+);
 projectRouter.get("/highlights", ProjectsController.getHighlights);
 projectRouter.get("/technologies", ProjectsController.getTechnologies);
 
-projectRouter.patch("/:id", ProjectsController.update);
+projectRouter.patch(
+  "/:id",
+  UtilsMiddlewares.validateId,
+  ProjectsMiddlewares.validateUpdatedProject,
+  ProjectsController.update
+);
 
-projectRouter.delete("/:id", ProjectsController.delete);
+projectRouter.delete(
+  "/:id",
+  UtilsMiddlewares.validateId,
+  ProjectsController.delete
+);
 
 app.use("/projects", projectRouter);
