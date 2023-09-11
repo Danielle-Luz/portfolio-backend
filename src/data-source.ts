ffa74f354fd03c17be8660b8e3afab11795118b5
Danielle Luz <daniln2209@gmail.com>
2023-09-11 00:43:29 -0300

    feat: add console.log in error handler and add database link

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ const dataSourceConfig = (): DataSourceOptions => {
   const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
   const migrationPath: string = path.join(__dirname, "./migrations/**.{ts,js}");
 
-  const dbUrl: string | undefined = process.env.DATABASE_URL;
+  const dbUrl: string | undefined = "postgres://fiyxmdxp:q9irVXp_hlJKknN6LEOvqhT4gFGRKDEq@silly.db.elephantsql.com/fiyxmdxp";
 
   if (!dbUrl) throw new Error("Missing env var: 'DATABASE_URL'");
 
