3a48dc8ad4e338edb7d2dbaeb287599066f75fd2
Danielle Luz <daniln2209@gmail.com>
2023-09-19 01:39:33 -0300

    feat: remove vercel config

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,8 +7,6 @@ const dataSourceConfig = (): DataSourceOptions => {
   const entitiesPath: string = path.join(__dirname, "/entities/**/*.{ts,js}");
   const migrationPath: string = path.join(__dirname, "/migrations/**/*.{ts,js}");
 
-  const dbUrl: string | undefined = process.env.DATABASE_URL;
-
   return {
     type: process.env.DB_TYPE as any,
     host: process.env.DB_HOST,
