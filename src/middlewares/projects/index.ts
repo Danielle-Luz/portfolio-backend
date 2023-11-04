32483b476f19dedb87d6770b838cb504b152e07a
Danielle Luz <daniln2209@gmail.com>
2023-11-04 10:17:51 -0300

    feat: create middleware to validate many projects

--- src/middlewares/projects/index.ts
+++ src/middlewares/projects/index.ts
@@ -1,14 +1,22 @@
 import { UtilsMiddlewares } from "../utils";
-import { newProjectSchema, updatedProjectSchema } from "../../schemas";
+import { manyNewProjectsSchema, newProjectSchema, updatedProjectSchema } from "../../schemas";
 import { requestStorageProperties } from "../../interfaces";
 
 export class ProjectsMiddlewares {
   static propertyToStoreValidatedData: requestStorageProperties = "project";
+  static propertyToStoreMultipleProjects: requestStorageProperties = "projects";
 
   static validateNewProject() {
     return UtilsMiddlewares.validateDataSchema(
       newProjectSchema,
       ProjectsMiddlewares.propertyToStoreValidatedData
+    );
+  }
+
+  static validateManyNewProject() {
+    return UtilsMiddlewares.validateDataSchema(
+      manyNewProjectsSchema,
+      ProjectsMiddlewares.propertyToStoreMultipleProjects
     );
   }
 
